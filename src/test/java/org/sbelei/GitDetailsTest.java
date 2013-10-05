e37dc796a497549c0b4717a9452018097f3b3de6
Sergiy Beley <sergiy.beley@gmail.com>
2013-10-05 21:12:12 +0300

    creates test env on fs. need only create branch and set up origin.

--- src/test/java/org/sbelei/GitDetailsTest.java
+++ src/test/java/org/sbelei/GitDetailsTest.java
@@ -3,20 +3,26 @@ package org.sbelei;
 import static org.junit.Assert.*;
 
 import java.io.File;
+
+import org.eclipse.jgit.lib.Repository;
+
 import org.junit.Test;
+
 import static org.sbelei.TestHelper.*;
 
 public class GitDetailsTest {
 	
 	@Test
 	public void testGetCurrentBranchString() throws Exception {
-		File temp = createTempDirectory();
-		//0. create folder 'not_repo'
-		//1. create git repo 'test_repo'
+		File temp = createTempFolder();
+		System.out.println(temp.getCanonicalPath());
+		createSubFolder(temp,"not_repo");	
+		Repository repo = createGitRepo(temp,"test_repo");
 		//2. add origin 'foo_bar'
 		//3. switch to branch 'branch_1'
 		//4. plugin should say that it can work with 'test_repo'
 		//5. plugin should get origin url and branch name
+		//deleteFolder(temp);
 	}
 
 
--- src/test/java/org/sbelei/TestHelper.java
+++ src/test/java/org/sbelei/TestHelper.java
@@ -3,9 +3,19 @@ package org.sbelei;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
 public class TestHelper {
-	
-	public static File createTempDirectory() throws IOException {
+
+	/**
+	 * Creates temp folder with name "tempXXXXXXX".
+	 * 
+	 * @return object that corresponds to physically created temp folder
+	 * @throws IOException
+	 *             in case it can't create it
+	 */
+	public static File createTempFolder() throws IOException {
 		final File temp;
 
 		temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
@@ -21,6 +31,44 @@ public class TestHelper {
 		}
 
 		return (temp);
+	}
+
+	/**
+	 * Removes folder and it's content.
+	 * 
+	 * @param folder
+	 *            to remove (be carefull)
+	 */
+	public static void deleteFolder(File folder) {
+		File[] files = folder.listFiles();
+		if (files != null) { // some JVMs return null for empty dirs
+			for (File f : files) {
+				if (f.isDirectory()) {
+					deleteFolder(f);
+				} else {
+					f.delete();
+				}
+			}
+		}
+		folder.delete();
+	}
+
+	public static Repository createGitRepo(File temp, String repoName) throws IOException {
+		// prepare a new folder
+		createSubFolder(temp, repoName);
+		// create the directory
+		Repository repository = FileRepositoryBuilder.create(new File(createSubFolder(temp, repoName), ".git"));
+		repository.create();
+
+		return repository;
+
+	}
+
+	public static File createSubFolder(File temp, String folderName) {
+		File subfolder = new File(temp, folderName);
+		subfolder.mkdir();
+		return subfolder;
+		
 	}
 
 }
