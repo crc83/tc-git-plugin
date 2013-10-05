961f3d43f13669a04442c7463277a106c938489c
Sergiy Beley <sergiy.beley@gmail.com>
2013-10-05 20:40:08 +0300

    add test skeleton to check if plugin works

---
+++ src/main/java/org/sbelei/GitDetails.java
@@ -0,0 +1,22 @@
+package org.sbelei;
+
+import plugins.wdx.FieldValue;
+import plugins.wdx.WDXPluginAdapter;
+
+public class GitDetails extends WDXPluginAdapter {
+
+	@Override
+	public int contentGetSupportedField(int fieldIndex, StringBuffer fieldName,
+			StringBuffer units, int maxlen) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int contentGetValue(String fileName, int fieldIndex, int unitIndex,
+			FieldValue fieldValue, int maxlen, int flags) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/test/java/org/sbelei/TestHelper.java
@@ -0,0 +1,26 @@
+package org.sbelei;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestHelper {
+	
+	public static File createTempDirectory() throws IOException {
+		final File temp;
+
+		temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
+
+		if (!(temp.delete())) {
+			throw new IOException("Could not delete temp file: "
+					+ temp.getAbsolutePath());
+		}
+
+		if (!(temp.mkdir())) {
+			throw new IOException("Could not create temp directory: "
+					+ temp.getAbsolutePath());
+		}
+
+		return (temp);
+	}
+
+}
